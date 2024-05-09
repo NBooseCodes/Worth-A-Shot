13986c55ff33fd5ac116e8bbd81a2128723668c2
Nicole Boose <nmboose@gmail.com>
2024-05-09 19:34:34 -0400

    Update tables.sql
    Added Foreign Key check fixes, added CASCADE functionality for both ON DELETE CASCADE and ON DELETE SET NULL

--- tables.sql
+++ tables.sql
@@ -1,15 +1,7 @@
---
--- Group 80 “Worth A Shot”: Nicole Boose and Elsa Luthi
--- CS 340, Spring 2024
--- Description: This file is the creation of our database for a company that sells alcohol. 
---
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
 
--- 
--- TABLE CREATION BELOW
--- 
-
--- Creates Wholesalers table that stores information on alcohol wholesalers.
-CREATE TABLE `Wholesalers` (
+CREATE OR REPLACE TABLE `Wholesalers` (
     `wholesalerID` int(11) AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `address` varchar(255) NOT NULL,
@@ -20,76 +12,58 @@ CREATE TABLE `Wholesalers` (
     PRIMARY KEY (`wholesalerID`)
 );
 
--- Creates Alcohols table that stores information different types of alcohol.
--- inventory: has to be updated with every sale and purchase
-CREATE TABLE `Alcohols` (
+CREATE OR REPLACE TABLE `Alcohols` (
     `alcoholID` int(11) AUTO_INCREMENT,
-    `alcoholName` varchar(255) NOT NULL,
+	`alcoholName` varchar(255) NOT NULL,
     `alcoholType` varchar(255) NOT NULL,
     `alcoholPercentage` decimal(19,4) NOT NULL,
     `wholesalePrice` decimal(19,4) NOT NULL,
     `alcoholVolume` decimal(19,4) NOT NULL,
-    `retailPrice` decimal(19,4) NOT NULL,
-    `inventory` int(11) NOT NULL DEFAULT 0,
+    `inventory` int(11),
     PRIMARY KEY (`alcoholID`)
 );
 
 -- Creates Emplpoyee table that stores information different employees in the organization.
-CREATE TABLE `Employees` (
+CREATE OR REPLACE TABLE `Employees` (
     `employeeID` int(11) AUTO_INCREMENT,
-	`employeeName` varchar(255) NOT NULL,
+	`firstName` varchar(255) NOT NULL,
+    `lastName` varchar(255) NOT NULL,
     `startDate` date NOT NULL,
     `employeeRole` varchar(255) NOT NULL,
     PRIMARY KEY (`employeeID`)
 );
 
--- Creates Sales table that stores information on an alcohol sale transaction.
--- totalCost: adds up all of the AlcoholSales(linePrice) for the specific sale.
-CREATE TABLE `Sales` (
-    `saleID` int(11) AUTO_INCREMENT,
-	`employeeID` int(11) NOT NULL,
-    `saleDate` date NOT NULL,
-    `saleTime` time NOT NULL,
-    `totalPrice` decimal(19,4) NOT NULL DEFAULT 0.00,
-    FOREIGN KEY (`employeeID`) REFERENCES `Employees`(`employeeID`),
-    PRIMARY KEY (`saleID`)
-);
 
--- Creates Purchases table that stores information on an alcohol purchasing transaction.
--- TotalPrice: add all of the AlcoholPurchases(lineCost) together for the specific purchase.
-CREATE TABLE `Purchases` (
+-- Creates Purchases table that stores information on an alcohol purchasing event.
+-- Moved totalCost to AlcoholPurchases
+-- TotalPrice: can be null for now add all of the AlcoholPurchases(linePrice) together for the specific purchase.
+CREATE OR REPLACE TABLE `Purchases` (
     `purchaseID` int(11) AUTO_INCREMENT,
     `wholesalerID` int(11) NOT NULL,
-	`paid` tinyint(1) NOT NULL DEFAULT 0,
+    `employeeID` int(11) NOT NULL,
+	`paid` tinyint(1) DEFAULT 0,
     `deliveryDate` date NOT NULL,
-    `delivered` tinyint(1) NOT NULL DEFAULT 0,
-    `totalCost` decimal(19,4) NOT NULL DEFAULT 0.00,
-    FOREIGN KEY (`wholesalerID`) REFERENCES `Wholesalers`(`wholesalerID`),
-    PRIMARY KEY (`purchaseID`)
-);
-
--- Creates AlcoholSales intersection table that stores information on the Sales of the Alcohols. 
--- linePrice: the quanitity sold * Alcohols(retailPrice)
-CREATE TABLE `AlcoholSales` ( 
-	`alcoholSaleID` int(11) AUTO_INCREMENT,
-    `saleID` int(11) NOT NULL,
-	`alcoholID` int(11) NOT NULL,
-    `quantitySold` int(11) NOT NULL,
-    `linePrice` decimal(19,4) NOT NULL,
-    FOREIGN KEY (`alcoholID`) REFERENCES `Alcohols`(`alcoholID`),
-    FOREIGN KEY (`saleID`) REFERENCES `Sales`(`saleID`),
-	PRIMARY KEY (`alcoholSaleID`)
+    `delivered` tinyint(1) DEFAULT 0,
+    `totalCost` decimal(19,4),
+    PRIMARY KEY (`purchaseID`),
+    FOREIGN KEY (`wholesalerID`) REFERENCES `Wholesalers`(`wholesalerID`) ON DELETE SET NULL,
+    FOREIGN KEY (`employeeID`) REFERENCES `Employees`(`employeeID`) ON DELETE SET NULL
 );
 
 -- Creates AlcoholPurchases intersection table that stores information on the Purchases of the Alcohols.
--- lineCost: the quanitity purchased * Alcohols(wholesalePrice)
-CREATE TABLE `AlcoholPurchases` (
+-- Removed AlcoholPurchaseID is it necissary?? 
+-- QuantityPurchased: updated name for consistency
+-- LineCost: need to figure out how to make this based on AlcoholPurchases(amount) * Alcohol(wholesalePrice)
+CREATE OR REPLACE TABLE `AlcoholPurchases` (
 	`alcoholPurchaseID` int(11) AUTO_INCREMENT,
     `purchaseID` int(11) NOT NULL,
 	`alcoholID` int(11) NOT NULL,
     `quantityPurchased` int(11) NOT NULL,
     `lineCost` decimal(19,4) NOT NULL,
     FOREIGN KEY (`alcoholID`) REFERENCES `Alcohols`(`alcoholID`),
-    FOREIGN KEY (`purchaseID`) REFERENCES `Purchases`(`purchaseID`),
+    FOREIGN KEY (`purchaseID`) REFERENCES `Purchases`(`purchaseID`) ON DELETE CASCADE,
     PRIMARY KEY (`alcoholPurchaseID`)
 );
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
